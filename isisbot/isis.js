039bbb6262de96ed5645f6accdb683be16be3a88
kernelzeroday <enktal@gmail.com>
2015-11-19 00:37:56 -0800

    tumblr support

---
+++ isisbot/isis.js
@@ -0,0 +1,35 @@
+//install: npm install -g twitter
+
+//usage: nodejs script.js keyword
+
+var tumblr = require('tumblr');
+
+var oauth = {
+  consumer_key: '',
+  consumer_secret: '',
+  token: '',
+  token_secret: ''
+};
+
+
+var Twitter = require('twitter');
+//https://apps.twitter.com/app/new for your api keys 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+client.stream('statuses/filter', {track: process.argv[2]}, function(stream){
+  stream.on('data', function(tweet) {
+//	if you want full json
+//   	console.log(tweet.text);
+        console.log('tweet id:',tweet.id, 'user id:', tweet.user.id, 'tweet: ', tweet.text);
+	console.log();
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
