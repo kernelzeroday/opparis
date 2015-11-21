7a7792959775ee8acfded57860be160c7218e0bd
kernelzeroday <enktal@gmail.com>
2015-11-20 16:06:01 -0800

    bitflip's python cloudflare tool

---
+++ bitflip.cloudflare.py
@@ -0,0 +1,24 @@
+python script that takes in a list of urls and prints out if they are behind cloud flare 
+
+file name: detect-cf.py
+useage: python detect-cf.py list_of_urls.txt
+notes: one url per line in the text file
+
+import requests
+import sys
+f = sys.argv[1]
+urls = open(f, 'r')
+print "(url): CloudFlare?"
+for u in urls.readlines():
+	u = u.rstrip()
+	try:
+		r = requests.get(u, verify=False)
+	except:
+		print "("+u+"): error"
+	try:
+		if("<title>Attention Required! | CloudFlare</title>" in str(r.text)):
+			print "("+u+"): yes"
+		else:
+			print "("+u+"): no"
+	except:
+		print "("+u+"): error"
