1073d2975979bae92d061d0a03cef6e8068a94a7
codezer0 <icc81401@diwaq.com>
2015-11-23 18:20:18 +0200

    Create twitteraccountscanner.py
    twitter account list scanner

---
+++ twitteraccountscanner.py
@@ -0,0 +1,84 @@
+#############################################################################################################
+# Twitter Account Scanner
+#
+# Project: #OpParis
+#
+# Author: codezer0
+# Start Date: 23/11/2015
+# Last Updates: 23/11/2015
+#
+# Version: 0.05
+#
+# Requirements:
+#     Python 2.7
+#
+# Usage:
+#     python twitteraccountscanner.py <name of list of hashtags>
+#
+# Given a list of twitter accounts, will scan and give a list with active and one with banned accounts.    
+###########################################################################################################
+import urllib2
+import httplib
+
+#The string-key to look for banned account page
+stringbanned = "<title>Twitter / Account Suspended</title>"
+#The string-key to look for active account page
+stringactive = "<meta name=\"description\" content=\"The latest Tweets from"
+
+#The list with the twitter accounts one per line
+inlist = "list.txt"
+#The file where the banned account are saved
+listbanned = "banned.txt"
+#The file where the active account are saved
+listactive = "active.txt"
+#The file where error logs are saved
+listerror = "errorlog.txt"
+
+i = 0
+iban = 0
+iactive = 0
+ierror = 0
+
+fban = open(listbanned,'w')
+factive = open(listactive,'w')
+ferror = open(listerror,'w')
+
+with open(inlist, "r") as ins:
+    array = []
+    for line in ins:
+	i += 1
+	print(i)
+        url = line
+	#Some error handling
+	try: 
+   	    html = urllib2.urlopen(url).read()
+	except urllib2.HTTPError, e:
+    	    print('HTTPError = ' + str(e.code) +" "+url)
+	    ferror.write('HTTPError = ' + str(e.code) +" "+url)
+	    ierror += 1
+	except urllib2.URLError, e:
+    	    print('URLError = ' + str(e.reason) +" "+url)
+	    ferror.write('URLError = ' + str(e.reason) +" "+url)
+	    ierror += 1
+	except httplib.HTTPException, e:
+    	    print('HTTPException' +" "+url)
+	    ferror.write('HTTPException' +" "+url)
+	    ierror += 1
+
+	if stringbanned in html:
+	    fban.write(url)
+            iban += 1
+        elif stringactive in html:
+	    factive.write(url)
+	    iactive += 1
+	else:
+	    ferror.write('Error = doesnt looks like a twitter profile '+ url)
+	    ierror += 1
+
+
+
+print("Scanned "+str(i)+" link(s) "+str(iban)+" banned profile(s) "+str(iactive)+" active profile(s) and got "+str(ierror)+" error(s)")
+
+fban.close()
+factive.close()
+ferror.close()
