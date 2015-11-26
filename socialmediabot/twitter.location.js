ec841dd45a6b15926fb31aa9674ec236e62a1d04
kod <enktal@gmail.com>
2015-11-26 14:47:23 -0800

    test location search

---
+++ socialmediabot/twitter.location.js
@@ -0,0 +1,24 @@
+var Twitter = require('twitter');
+//https://apps.twitter.com/app/new for your api keys 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+client.get('statuses/user_timeline', {screen_name: process.argv[2]}, function(error, tweets, response){
+  stream.on('data', function(tweet) {
+//      if you want full json
+//      console.log(tweet.text);
+        console.log('tweet id:',tweet.id, 'user id:', tweet.user.id, 'tweet: ', tweet.text, geo, user.location, coordinates  );
+        console.log();
+//      client.say('tweet id:',tweet.id, 'user id:', tweet.user.id, 'tweet: ', tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+
+
